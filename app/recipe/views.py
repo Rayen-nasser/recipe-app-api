7e9573035dd3377009f5a81ee5c24c641905ed3f
rayen <rayenn38@gmail.com>
2024-12-27 17:11:03 +0100

    change get recipes to be need authentication

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,25 +1,23 @@
+"""
+View for the recipe APIs
+"""
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import Recipe
-from recipe.serializers import RecipeSerializer
-
+from recipe.serializers import RecipeSerializer  # Corrected import statement
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """
     ViewSet for listing, creating, retrieving, updating, and deleting recipes.
     """
     queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
+    serializer_class = RecipeSerializer  # Using the imported serializer
     authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
-    # Custom permission logic
-    def get_permissions(self):
-        if self.action == 'list':  # Allow unauthenticated users to access the list
-            return []  # No permissions required for 'GET' request (list)
-
-        # Enforce authentication for other actions (POST, PUT, DELETE)
-        return [
-            IsAuthenticated(),
-        ]
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
