f969c7959a65b6246c6f862a7194d06d041afcae
rayen <rayenn38@gmail.com>
2024-12-28 09:41:56 +0100

    implemnet create recipe and recipe detail

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -11,3 +11,10 @@ class RecipeSerializer(serializers.ModelSerializer):
             'id', 'title', 'description', 'time_minutes', 'price', 'link'
         ]
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(serializers.ModelSerializer):
+    """Serializer for recipe objects with extra details."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,10 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import Recipe
-from recipe.serializers import RecipeSerializer  # Corrected import statement
+from recipe.serializers import (
+    RecipeSerializer,
+    RecipeDetailSerializer
+)
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -14,10 +17,20 @@ class RecipeViewSet(viewsets.ModelViewSet):
     ViewSet for listing, creating, retrieving, updating, and deleting recipes.
     """
     queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer  # Using the imported serializer
+    serializer_class = RecipeDetailSerializer  # Using the imported serializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer for the authenticated user."""
+        if self.action == 'list':
+            return RecipeSerializer
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
